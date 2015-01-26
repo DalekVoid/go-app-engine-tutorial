cd31fa7c65f26e1179a65e7879ceadb4d1b8babd
Dalek Void <dalek@voidamplifier.com>
2015-01-26 19:30:32 +0800

    simple login

--- main.go
+++ main.go
@@ -3,6 +3,9 @@ package main
 import (
   "fmt"
   "net/http"
+
+  "appengine"
+  "appengine/user"
 )
 
 func init() {
@@ -10,5 +13,19 @@ func init() {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprint(w, "Hello, World!")
+  // fmt.Fprint(w, "Hello, World!")
+
+  c := appengine.NewContext(r)
+  u := user.Current(c) // if user has already logged in
+  if u == nil {
+    url, err := user.LoginURL(c, r.URL.String())
+    if err != nil {
+      http.Error(w, err.Error(), http.StatusInternalServerError)
+      return
+    }
+    w.Header().Set("Location", url)
+    w.WriteHeader(http.StatusFound)
+    return
+  }
+  fmt.Fprintf(w, "Hello, %v!", u)
 }
